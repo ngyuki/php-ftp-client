be7edc62150c53fa34f7fcae72ba17bb5170e059
tsyk goto <ngyuki.ts@gmail.com>
2012-09-05 14:24:16 +0900

    Win用バッチファイル pushd popd

--- run-phpdoc.bat
+++ run-phpdoc.bat
@@ -1,4 +1,8 @@
 @echo off
 
+pushd %~dp0
+
 call phpdoc -d class -t phpdoc --template=abstract
 start phpdoc/index.html
+
+popd
--- run-phpunit.bat
+++ run-phpunit.bat
@@ -1,4 +1,8 @@
 @echo off
 
+pushd %~dp0
+
 call phpunit -c tests/phpunit-win.xml --coverage-html report/ tests/
 start report/index.html
+
+popd
